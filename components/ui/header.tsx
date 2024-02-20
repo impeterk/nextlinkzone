65eb5621d287fec86ffdbaed915f1dd75a0b0d89
Peter <storm.sk123@gmail.com>
2024-02-20 16:55:31 +0100

    fix imports

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,8 +1,8 @@
 import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { LogOut, Logo } from '@/components/buttons'
 import NavLinks from "./nav-links";
-import CreateLink from "@/app/(main)/create/CreateLink";
-import LoginLink from "@/app/(main)/login/LoginLink";
+import CreateLink from "@/app/(app)/create/CreateLink";
+import LoginLink from "@/app/(app)/login/LoginLink";
 
 export default function Header() {
   const user = true
