0d22bc766c052e589d5d8597e401e076aec1241e
Peter Kudelas <storm.sk123@gmail.com>
2024-09-02 10:01:05 +0000

    add million js

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,25 @@
+import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'avatars.githubusercontent.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'utfs.io',
+      port: '',
+      pathname: '/**'
+    }]
+  }
 };
-
-export default nextConfig;
+export default MillionLint.next({
+  rsc: true
+})(nextConfig);
